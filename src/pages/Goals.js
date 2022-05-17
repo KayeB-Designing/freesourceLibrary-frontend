c867fb05b4614e7e816795c8820b3c6a8724029b
Kaye Bryant <bryantk72391@gmail.com>
2022-05-17 15:37:52 -0400

    testing api fetch

--- src/pages/Goals.js
+++ src/pages/Goals.js
@@ -10,14 +10,14 @@ function Goals(props) {
   const [click, setClick] = useState(false)
 
 
-  // const myHeaders = new Headers();
-  // myHeaders.append("Content-Type", "application/json");
-  // const requestOptions = {
-  //     method: "get",
-  //     headers: myHeaders,
-  //     redirect: "follow",
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  const requestOptions = {
+      method: "get",
+      headers: myHeaders,
+      redirect: "follow",
       
-  // };
+  };
 
 
   const getGoalsData = async () => {
@@ -79,12 +79,12 @@ function Goals(props) {
   //   return created()
   // }
 
-  // function fetchPdf() {
-  //   fetch(`https://v1.nocodeapi.com/kayebedesigning/pdf/WKgezVWsvcSbNnwO/url2pdf?url=${props.URL}/goals`, requestOptions)
-  //   .then(response => response.text())
-  //   .then(result => console.log(`this is the fetch result: ${result}`))
-  //   .catch(error => console.log('error', error));
-  // }
+  function getPdf() {
+    fetch(`https://v1.nocodeapi.com/kayebedesigning/pdf/WKgezVWsvcSbNnwO/url2pdf?url=${props.URL}/goals`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(`this is the fetch result: ${result}`))
+    .catch(error => console.log('error', error));
+  }
 
   // function buttonClicked() {
   //   console.log(`clicked`)
@@ -93,8 +93,9 @@ function Goals(props) {
   // }
 
 
-  const hello = () => {
-    console.log(`hello`)
+  const clicked = () => {
+    console.log(`clicked`)
+    getPdf()
     setClick()
   }
 
@@ -116,7 +117,7 @@ function Goals(props) {
         {/* <button onClick={created()}>Download & Print PDF</button> */}
         {/* <button onClick={console.log(`this button has been clicked`)}>Download & Print PDF</button> */}
         {/* <button onClick={buttonClicked()}>Download & Print PDF</button> */}
-        <button onClick={hello}>Download & Print PDF</button>
+        <button onClick={clicked}>Download & Print PDF</button>
         {/* onClick={() => setPDF()} */}
       </div>
       <div>
