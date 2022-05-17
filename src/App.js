aada1d913185d9e3c8112a455ccb7316846d150d
Kaye Bryant <bryantk72391@gmail.com>
2022-05-17 17:17:57 -0400

    testing api fetch

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import About from "./pages/About";
 import Goals from "./pages/Goals";
 import Habits from "./pages/Habits";
 import TimeManagement from "./pages/TimeManagement";
+import GoalsPDF from './pages/GoalsPDF';
+import HabitsPDF from './pages/HabitsPDF';
+import TimeManagementPDF from './pages/TimeManagementPDF';
 
 
 
@@ -25,8 +28,11 @@ function App() {
         <Route path="/home" element={< Home />}/>
         <Route path="/about" element={< About URL={URL}/>}/>
         <Route path="/goals" element={< Goals URL={URL}/>}/>
+        <Route path="/goals/pdf" element={< GoalsPDF URL={URL}/>}/>
         <Route path="/habits" element={< Habits URL={URL}/>}/>
+        <Route path="/habits/pdf" element={< HabitsPDF URL={URL}/>}/>
         <Route path="/timeManagement" element={< TimeManagement URL={URL}/>}/>
+        <Route path="/timeManagement/pdf" element={< TimeManagementPDF URL={URL}/>}/>
       </Routes>
       <Footer />
 
