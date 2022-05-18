58421142fa674a0fbcc0f815852caceb8d4ee55a
Kaye Bryant <bryantk72391@gmail.com>
2022-05-18 11:03:39 -0400

    testing api

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={< Home />}/>
         <Route path="/home" element={< Home />}/>
-        <Route path="/index.html" element={< Home />}/>
+        <Route path="/index.html" element={< Goals />}/>
         <Route path="/about" element={< About URL={URL}/>}/>
         <Route path="/goals" element={< Goals URL={URL}/>}/>
         {/* <Route path="/goals/pdf" element={< GoalsPDF URL={URL}/>}/> */}
