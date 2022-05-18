f7e134ebb4ee0cb8e1af08e762f6ef3872b6f377
Kaye Bryant <bryantk72391@gmail.com>
2022-05-18 13:14:52 -0400

    testing api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import About from "./pages/About";
-import Goals from "./pages/Goals";
-import Habits from "./pages/Habits";
-import TimeManagement from "./pages/TimeManagement";
+import About from "../pages/About";
+import Goals from "../pages/Goals";
+import Habits from "../pages/Habits";
+import TimeManagement from "../pages/TimeManagement";
 
 export default function Main() {
     const URL = "https://freesource-library.herokuapp.com";
