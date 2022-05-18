038b9b91404b693a6aaee172adb3c8b505575b6d
Kaye Bryant <bryantk72391@gmail.com>
2022-05-18 13:09:34 -0400

    testing api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from './components/Main';
 // import { Route, Routes } from "react-router-dom";
 // import Home from "./pages/Home";
 // import About from "./pages/About";
