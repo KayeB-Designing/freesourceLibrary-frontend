274f7040703e16939694cfe9ad54df8b40ce2296
Kaye Bryant <bryantk72391@gmail.com>
2022-05-18 14:10:08 -0400

    testing api

---
+++ src/components/Message.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Message() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'quotes15.p.rapidapi.com',
+      'X-RapidAPI-Key': '7dc5c7d94amsh5950acede55f00ap1606fbjsncf5bef6f77af'
+    }
+  };
+  
+  fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Message from './Message';
+import Message from './components/Message';
 
 import { BrowserRouter as Router } from "react-router-dom";
 
