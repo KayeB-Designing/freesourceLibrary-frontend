2980e9fce4446486f76d23c5b1d5009e37f9e0fe
Kaye Bryant <bryantk72391@gmail.com>
2022-05-18 13:53:23 -0400

    testing api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Main from './components/Main';
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Goals from "./pages/Goals";
-// import Habits from "./pages/Habits";
-// import TimeManagement from "./pages/TimeManagement";
+// import Main from './components/Main';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Goals from "./pages/Goals";
+import Habits from "./pages/Habits";
+import TimeManagement from "./pages/TimeManagement";
 // import Message from './Message';
 // import GoalsPDF from './pages/GoalsPDF';
 // import HabitsPDF from './pages/HabitsPDF';
@@ -18,26 +18,22 @@ import Main from './components/Main';
 
 function App() {
 
-  // const URL = "https://freesource-library.herokuapp.com";
+  const URL = "https://freesource-library.herokuapp.com";
   // const apiURL = "https://v1.nocodeapi.com/kayebedesigning/pdf/WKgezVWsvcSbNnwO/url2pdf?url=";
   // const URL = "http://localhost:4000";
 
   return (
     <div className="App">
       <Header />
-      <Main />
-      {/* <Routes> */}
-        {/* <Route exact path="/" element={< Home />}/> */}
-        {/* <Route path="/home" element={< Home />}/> */}
-        {/* <Route path="/index.html" element={< Goals />}/> */}
-        {/* <Route path="/about" element={< About URL={URL}/>}/> */}
-        {/* <Route path="/goals" element={< Goals URL={URL}/>}/> */}
-        {/* <Route path="/goals/pdf" element={< GoalsPDF URL={URL}/>}/> */}
-        {/* <Route path="/habits" element={< Habits URL={URL}/>}/> */}
-        {/* <Route path="/habits/pdf" element={< HabitsPDF URL={URL}/>}/> */}
-        {/* <Route path="/timeManagement" element={< TimeManagement URL={URL}/>}/> */}
-        {/* <Route path="/timeManagement/pdf" element={< TimeManagementPDF URL={URL}/>}/> */}
-      {/* </Routes> */}
+      {/* <Main /> */}
+      <Routes>
+        <Route exact path="/" element={< Home />}/>
+        <Route path="/home" element={< Home />}/>
+        <Route path="/about" element={< About URL={URL}/>}/>
+        <Route path="/goals" element={< Goals URL={URL}/>}/>
+        <Route path="/habits" element={< Habits URL={URL}/>}/>
+        <Route path="/timeManagement" element={< TimeManagement URL={URL}/>}/>
+      </Routes>
       <Footer />
 
 
