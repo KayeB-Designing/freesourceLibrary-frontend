4ba033d2f90e965ea4c7e5c8f4d5ad82b8b10101
Kaye Bryant <bryantk72391@gmail.com>
2022-05-18 16:32:40 -0400

    testing api

--- src/components/Message.js
+++ src/components/Message.js
@@ -41,11 +41,9 @@ export default function Message() {
       return (
         <div>
           <h2>Quote of the day:</h2>
-          <p>
             {message.content}
+            {message.name}
             <a target="_blank" href="https://v1.nocodeapi.com/kayebedesigning/pdf/WKgezVWsvcSbNnwO/url2pdf?url=https://freesourcelibrary.netlify.app/home/&inline=Download&document_size=Letter&orientation=portrait" download><button onClick={buttonClicked}>Get an extra quote!</button></a>
-          </p>
-          {/* <p>{message.author}</p> */}
         </div>
       )
   }
